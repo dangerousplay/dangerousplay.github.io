5395fd1fc97fe1bab8151dc0508b9e822719c46a
Dangerousplay <dangerousplay715@gmail.com>
2023-09-16 20:34:38 -0300

    [Changed] Social media links and name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,22 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import remarkMath from "remark-math";
+import rehypeMathjaxChtml from 'rehype-mathjax/chtml.js'
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
+  site: 'https://dangerousplay.github.io',
+  integrations: [mdx(), sitemap(), tailwind()],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [
+            [rehypeMathjaxChtml, {
+                chtml: {
+                    fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
+                }
+            }]
+        ],
     }
-  )]
 });
